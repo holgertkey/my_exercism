0002d9dd9d687c25396591d119a8440d00d96d35
holgertkey <holgertkey@gmail.com>
2025-06-09 10:10:44 +0300

    sorting room 1

---
+++ go/sorting-room/sorting_room.go
@@ -0,0 +1,71 @@
+package sorting
+
+import (
+    "fmt"
+    "strconv"
+)
+
+// DescribeNumber should return a string describing the number.
+func DescribeNumber(f float64) string {
+    return fmt.Sprintf("This is the number %.1f", f)
+}
+
+type NumberBox interface {
+    Number() int
+}
+
+// DescribeNumberBox should return a string describing the NumberBox.
+func DescribeNumberBox(nb NumberBox) string {
+    return fmt.Sprintf("This is a box containing the number %.1f", float64(nb.Number()) )
+}
+
+type FancyNumber struct {
+    n string
+}
+
+func (i FancyNumber) Value() string {
+    return i.n
+}
+
+type FancyNumberBox interface {
+    Value() string
+}
+
+// ExtractFancyNumber should return the integer value for a FancyNumber
+// and 0 if any other FancyNumberBox is supplied.
+func ExtractFancyNumber(fnb FancyNumberBox) int {
+    var str string
+    if fn, ok := fnb.(FancyNumber); ok {
+        str = fn.Value()
+    }
+    if num, err := strconv.Atoi(str); err == nil {
+        return num
+    } else {
+        return 0
+    }
+}
+
+// DescribeFancyNumberBox should return a string describing the FancyNumberBox.
+func DescribeFancyNumberBox(fnb FancyNumberBox) string {
+    if fn, ok := fnb.(FancyNumber); ok {
+        return fmt.Sprintf("This is a fancy box containing the number %.1f", float64(ExtractFancyNumber(fn)) ) 
+    } else {
+        return fmt.Sprintf("This is a fancy box containing the number %.1f", 0.0 ) 
+    }   
+}
+
+// DescribeAnything should return a string describing whatever it contains.
+func DescribeAnything(i interface{}) string {
+    switch value := i.(type) {
+    case int:
+        return DescribeNumber(float64(value))
+    case float64:
+        return DescribeNumber(value)
+    case NumberBox:
+        return DescribeNumberBox(value)
+    case FancyNumberBox:
+        return DescribeFancyNumberBox(value)
+    default:
+        return "Return to sender"
+    }
+}
